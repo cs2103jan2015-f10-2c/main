0b529b581b7eae06699b27794196bf0aa19d9254
Shri Kishen Rajendran <sanjukishen@gmail.com>
2015-03-08 14:18:54 +0800

    writeDataOntoFile defined with change in return type

--- iPlannerLogic/iPlannerLogic/Logic.cpp
+++ iPlannerLogic/iPlannerLogic/Logic.cpp
@@ -8,3 +8,18 @@ Logic::Logic()
 Logic::~Logic()
 {
 }
+
+int Logic::writeDataOntoFile(char * fileName) {
+  // Variable to denote successful processing of function
+  int retCode = -1;
+  ofstream outfile(fileName);
+
+  if (!outfile.bad()) {
+    
+    outfile << _taskName << endl;
+    outfile.close();
+    retCode = 0;
+  }
+  
+  return retCode;
+}
