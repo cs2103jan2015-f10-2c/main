edf24d3dbfb6a115c9793d6c9ceadc4278a23ec5
Lee Joon Fai <a0108462@u.nus.edu>
2015-03-08 15:30:12 +0800

    TStorage: added CommandAdd, CommandEdit, ComandDelete as sub-classes of UndoableCommands

--- iPlannerTStorage/CommandDelete/CommandDelete.h
+++ iPlannerTStorage/CommandDelete/CommandDelete.h
@@ -2,15 +2,15 @@
 //	Tutorial Group 8 (Friday, 10:00AM); Group 2C
 //	Coder:	Lee Joon Fai, A0108462J
 
-#ifndef COMMAND_ADD_H
-#define COMMAND_ADD_H
+#ifndef COMMAND_DELETE_H
+#define COMMAND_DELETE_H
 
 #include <string>
 #include "..\UndoableCommand\UndoableCommand.h"
 
 using namespace std;
 
-class CommandAdd : UndoableCommand {
+class CommandDelete : UndoableCommand {
 public:
 	string executeCommand();
 	string undoCommand();
