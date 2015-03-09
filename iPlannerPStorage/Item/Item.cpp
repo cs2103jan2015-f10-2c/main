3f7b09bb849fc2de8535a6e72d6c3f6980824f12
Andy Soh Wei Zhi <andysohwz@gmail.com>
2015-03-09 11:47:03 +0800

    Storage: DateTime class: Overloaded constructors.
    Storage: Item class: Setters coded.

--- iPlannerPStorage/Item/Item.cpp
+++ iPlannerPStorage/Item/Item.cpp
@@ -3,3 +3,54 @@
 //	Coder:	Andy Soh Wei Zhi, A0112042E
 
 #include "Item.h"
+
+//	Constructor
+Item::Item() { }
+
+//	Destructor
+Item::~Item() { }
+
+string Item::setItemName(string itemName) {
+	_itemName = itemName;
+	return _itemName;
+}
+
+string Item::setDescription(string description) {
+	_description = description;
+	return _description;
+}
+
+DateTime Item::setStartTime(DateTime startTime) {
+	_startTime = startTime;
+	return _startTime;
+}
+
+DateTime Item::setEndTime(DateTime endTime) {
+	_endTime = endTime;
+	return _endTime;
+}
+
+unsigned int Item::setItemID(unsigned int itemID) {
+	_itemID = itemID;
+	return _itemID;
+}
+
+char Item::setPriority(char priority) {
+	_priority = priority;
+	return _priority;
+}
+
+char Item::setLabel(char label) {
+	_label = label;
+	return _label;
+}
+
+bool Item::setCompletion(bool isCompleted) {
+	_isCompleted = isCompleted;
+	return _isCompleted;
+}
+
+
+
+
+
