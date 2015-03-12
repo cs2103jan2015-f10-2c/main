57514581664e360c832444d06d4635ec7481c355
Lee Joon Fai <a0108462@u.nus.edu>
2015-03-12 16:44:51 +0800

    DateTimeVerification now validates more permutations of DateTime formats

--- iLogic/DateTimeVerification.h
+++ iLogic/DateTimeVerification.h
@@ -8,7 +8,24 @@ using namespace std;
 
 class DateTimeVerification {
 private:
+	static const int MIN_YEAR = 0;
+	static const int MAX_YEAR = 9999;
+	static const int MIN_MONTH = 1;
+	static const int MAX_MONTH = 12;
+	static const int MIN_DAY = 1;
+	static const int MAX_DAY = 31;
+	static const int MIN_HOUR = 0;
+	static const int MAX_HOUR = 23;
+	static const int MIN_MINUTE = 0;
+	static const int MAX_MINUTE = 59;
+	static const int EMPTYFIELD_DATETIME;
+
 	DateTime _dateTimeObjectToVerify;
+	int _year;
+	int _month;
+	int _day;
+	int _hour;
+	int _minute;
 
 	bool isValidYearRange();
 	bool isValidMonthRange();
@@ -17,17 +34,11 @@ private:
 	bool isValidHourRange();
 	bool isValidMinuteRange();
 
-	static const int MIN_YEAR = 0;
-	static const int MAX_YEAR = 9999;
-	static const int MIN_MONTH = 1;
-	static const int MAX_MONTH = 12;
-	static const int MIN_DAY = 1;
-	static const int MAX_DAY = 31;
-
-	static const int MIN_HOUR = 0;
-	static const int MAX_HOUR = 23;
-	static const int MIN_MINUTE = 0;
-	static const int MAX_MINUTE = 59;
+	bool hasYear();
+	bool hasMonth();
+	bool hasDay();
+	bool hasHour();
+	bool hasMinute();
 
 public:
 	DateTimeVerification(DateTime dateTimeObject);
