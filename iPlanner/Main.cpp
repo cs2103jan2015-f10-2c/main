44fa3c0778ac2c2c3085f8d4f37133782c3f3377
Ng Chon Beng <a0111238@nus.edu.sg>
2015-03-12 22:12:50 +0800

    iPlanner: minor changes for V0.1

--- iPlanner/Main.cpp
+++ iPlanner/Main.cpp
@@ -4,13 +4,13 @@
 void main() {
 	string input;
 	Logic testLogic;
-	cout << "Command : ";
+	cout << "Command: ";
 	while (getline(cin, input)) {
 		iParser testParser;
 		ParseInfo testParseInfo;	
 		testParseInfo = testParser.parse(input);
 		testLogic.initiateCommandAction(testParseInfo);
-		cout << "Command : ";
+		cout << "Command: ";
 	}
 	return;
 }
