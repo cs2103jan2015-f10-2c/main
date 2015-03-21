519dcf218e7c23714647dcb28645c235c5afe5a4
Lee Joon Fai <leejoonfai21@gmail.com>
2015-03-21 17:28:31 +0800

    DateTimeVerification: touchups to the class. pending unit testing before can confirm if it works properly

--- iLogic/DateTimeVerification.h
+++ iLogic/DateTimeVerification.h
@@ -7,7 +7,9 @@
 using namespace std;
 
 class DateTimeVerification {
+
 private:
+
 	static const int MIN_YEAR;
 	static const int MAX_YEAR;
 	static const int MIN_MONTH;
@@ -15,10 +17,23 @@ private:
 	static const int MIN_DAY;
 	static const int MAX_DAY;
 	static const int MIN_HOUR;
-	static const int MAX_HOUR ;
+	static const int MAX_HOUR;
 	static const int MIN_MINUTE;
 	static const int MAX_MINUTE;
 	static const int EMPTYFIELD_DATETIME;
+
+	static const int DAY_29;
+	static const int DAY_30;
+	static const int DAY_31;
+	static const int MONTH_FEB;
+	static const int MONTH_APR;
+	static const int MONTH_JUN;
+	static const int MONTH_SEP;
+	static const int MONTH_NOV;
+	static const int YEAR_4;
+	static const int YEAR_100;
+	static const int YEAR_400;
+
 
 	DateTime _dateTimeObjectToVerify;
 	int _year;
@@ -27,7 +42,10 @@ private:
 	int _hour;
 	int _minute;
 
-
+	/////////////////////////////
+	//REMOVE THIS AFTER TESTING//
+	////////////////////////////
+public:
 	// LEVEL 1 of SLAP
 	// This group of functions only check for VALIDITY of
 	// INDIVIDUAL year/month/day/hour/minute values.
@@ -46,7 +64,7 @@ private:
 	// individual year/month/day values come together as
 	// a valid date format.
 	// i.e. DD MM YYYY or DD MM -1
-	bool hasYearMonthDay();	
+	bool hasYearMonthDay();
 	bool hasMonthDay();
 	bool hasYear();
 	bool hasMonth();
@@ -56,12 +74,13 @@ private:
 	bool hasHour();
 	bool hasMinute();
 
+	bool isActualYearMonthDayDate();
+	bool isValidTime();
+
 public:
+
 	DateTimeVerification(DateTime dateTimeObject);
 
-	bool hasDate();
-	bool isValidDate();
-	bool isValidTime();
 	bool isValidDateTime();
 
 };
