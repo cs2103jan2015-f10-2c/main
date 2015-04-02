0b591b9c8c93a929249fb2c1f28e9b287661630e
Andy Soh Wei Zhi <andysohwz@gmail.com>
2015-04-03 02:44:08 +0800

    Storage: History: Implementation of History.

--- iStorage/History.cpp
+++ iStorage/History.cpp
@@ -3,9 +3,54 @@
 
 #include "History.h"
 
-//	Constructor
+const string History::COMMAND_ADD = "ADD";
+const string History::COMMAND_DELETE = "DELETE";
+const string History::COMMAND_REPLACE = "REPLACE";
+const string History::ERROR_ADD = "ERROR: Command and Item were not recorded.";
+const string History::ERROR_EMPTYSTACKS = "ERROR: Undo has reached its limit.";
+
+//	Default Constructor
 History::History() {}
 
-//	Destructor
+//	Default Destructor
 History::~History() {}
 
+bool History::isValidHistoryCommand(string command) {
+	if (command == COMMAND_ADD || command == COMMAND_DELETE || command == COMMAND_REPLACE) {
+		return true;
+	}
+
+	return false;
+}
+
+string History::addCommand(string command, Item item) {
+	if (isValidHistoryCommand(command)) {
+		_commandStack.push(command);
+		_itemStack.push(item);
+
+		return (command + item.displayItemFullDetails());
+	}
+
+	return ERROR_ADD;
+}
+
+bool History::isValidUndoCall() {
+	if (_commandStack.empty() || _itemStack.empty()) {
+		return false;
+	}
+	return true;
+}
+
+string History::undoLastCommand(string& command, Item& latestItem) {
+	if (isValidUndoCall()) {
+		return ERROR_EMPTYSTACKS;
+	} else {
+		command = _commandStack.top();
+		_commandStack.pop();
+
+		latestItem = _itemStack.top();
+		_itemStack.pop();
+
+		return (command + latestItem.displayItemFullDetails());
+	}
+}
--- iStorage/History.h
+++ iStorage/History.h
@@ -9,12 +9,34 @@
 
 class History {
 private:
+	static const string COMMAND_ADD;
+	static const string COMMAND_DELETE;
+	static const string COMMAND_REPLACE;
+	static const string ERROR_ADD;
+	static const string ERROR_EMPTYSTACKS;
+
 	stack <string> _commandStack;
 	stack <Item> _itemStack;
 
+	bool isValidHistoryCommand(string);
+	bool isValidUndoCall();
+
 public:
+	//	Default Constructor
+	History();
+
+	//	Default Destructor
+	~History();
+
+	//	Adds commands that modifies storage information (add, delete, edit)
+	//	Pre:	Valid item
+	//	Post:	Command is added into the command stack; item is added into the item stack
 	string addCommand(string, Item);
-	Item undoLastCommand();
+	
+	//	Removes commands from the item and command stack; returns item and command (both via reference)
+	//	Pre:	Item and command stacks are not empty
+	//	Post:	Command and Item are returned via reference; success/failure message is returned
+	string undoLastCommand(string&, Item& latestItem);
 };
 
 #endif
